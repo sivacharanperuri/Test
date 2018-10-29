92b2450cd8b558ad0ecd27095ea5a61eb8835f5e
Siva Charan <Siva Charan@LAPTOP-G02OPBD4>
2018-10-29 23:38:25 +0530

    adjustment in code

--- FirstProject/src/day1/examples/ExampleAndOr.java
+++ FirstProject/src/day1/examples/ExampleAndOr.java
@@ -3,9 +3,7 @@ package day1.examples;
 public class ExampleAndOr {
 
 	public static void main(String[] args) {
-		int x,y;
-		x=10;
-		y=-10;
+		int x,y; x=10;   y=-10;
 		if(x>0&&y>0)
 		{
 			System.out.println("Both numbers are positive");
